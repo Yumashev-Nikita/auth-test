5f07b3112224bb67a682dd069ef7018049b23dcb
L1brA7 <umashev58@gmail.com>
2022-06-15 19:14:46 +0300

    developments

--- src/components/AuthWindow.vue
+++ src/components/AuthWindow.vue
@@ -3,26 +3,61 @@
   .auth-window__label.signIn-text Войти
   .auth-window__input-container
     span(class='input-bar-text') Логин
-    input(class='auth-window__login')
+    textarea(class='auth-window__login input-bar-text' placeholder='Ваш логин'
+    v-model='name')
   .auth-window__input-container
     span(class='input-bar-text') Пароль
-    input(class='auth-window__password')
+    textarea(class='auth-window__password input-bar-text' placeholder='Ваш пароль'
+    v-model='password')
+  span(class='commit-text' @click='sendAuth') Подтвердить
 </template>
 
 <script>
 export default {
   name: 'AuthWindow',
+  data() {
+    return {
+      password: '',
+      login: '',
+      name: '',
+      email: '',
+      type: '',
+      github: '',
+      city: '',
+      phone: '',
+      birthday: '',
+    };
+  },
+  methods: {
+    async sendAuth() {
+      const res = await fetch('https://test.atwinta.online/api/v1/auth/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: this.name,
+          email: 'umashev58@gmail.com',
+          type: 'front',
+          github: 'https://github.com/Yumashev-Nikita',
+          city: 'Кемерово',
+          phone: '9964150932',
+          birthday: '05.06.2001',
+        }),
+      });
+      console.log(res.body);
+    },
+  },
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang='sass' scoped>
 @use './style/textstyles'
 
 .auth-window
   margin: 200px auto
   padding: 50px
   width: 500px
-  height: 200px
   border: 2px solid black
   border-radius: 15px
   display: flex
@@ -32,10 +67,20 @@ export default {
     display: flex
     flex-direction: column
     justify-content: space-between
-    height: 60px
   &__label
+    margin-bottom: 20px
   &__login
-    height: 20px
+    margin: 10px 0
+    max-height: 24px
+    padding: 5px 5px
+    border: 1px solid black
+    border-radius: 7px
+    resize: none
   &__password
-    height: 20px
+    margin: 10px 0
+    max-height: 24px
+    padding: 5px 5px
+    border: 1px solid black
+    border-radius: 7px
+    resize: none
 </style>
